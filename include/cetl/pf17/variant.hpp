850561155f942b6dbf90ca3ed8fd5b22bd7ad328
Pavel Kirienko <pavel.kirienko@gmail.com>
2024-02-22 15:52:38 +0200

    Add SMF policies, but this actually seems like a dead end; will need visitor next

--- include/cetl/pf17/variant.hpp
+++ include/cetl/pf17/variant.hpp
@@ -54,11 +54,11 @@ struct types final
     static constexpr bool all_satisfy = conjunction_v<F<Ts>...>;
 
     static constexpr bool trivially_destructible       = all_satisfy<std::is_trivially_destructible>;
-    static constexpr bool trivially_copyable           = all_satisfy<std::is_trivially_copyable>;
-    static constexpr bool trivially_copy_assignable    = all_satisfy<std::is_trivially_copy_assignable>;
-    static constexpr bool trivially_move_assignable    = all_satisfy<std::is_trivially_move_assignable>;
     static constexpr bool trivially_copy_constructible = all_satisfy<std::is_trivially_copy_constructible>;
     static constexpr bool trivially_move_constructible = all_satisfy<std::is_trivially_move_constructible>;
+    static constexpr bool trivially_copy_assignable    = all_satisfy<std::is_trivially_copy_assignable>;
+    static constexpr bool trivially_move_assignable    = all_satisfy<std::is_trivially_move_assignable>;
+    static constexpr bool nothrow_move_constructible   = all_satisfy<std::is_nothrow_move_constructible>;
 
     types()  = delete;
     ~types() = delete;
@@ -87,110 +87,32 @@ static_assert(0 == index_of<int, int, double, char>, "");
 static_assert(1 == index_of<double, int, double, char>, "");
 static_assert(2 == index_of<char, int, double, char>, "");
 
-/// This type manages the storage arena for the variant and keeps track of the currently active type.
-/// It expressly ignores copy/move/destruction/assignment; this must be managed externally.
-/// We support the valueless semantics even if __cpp_exceptions is not defined for the sake of ABI compatibility
-/// because there may be another translation unit that uses exceptions.
-template <typename... Ts>
-class arena final
-{
-public:
-    template <std::size_t N, typename T = nth_type<N, Ts...>, typename... Args>
-    explicit arena(const in_place_index_t<N>, Args&&... args)  // NOLINT(*-pro-type-member-init)
-        : m_index(N)
-        , m_dtor([](void* const data) { reinterpret_cast<T*>(data)->~T(); })
-    {
-        new (m_data) T(std::forward<Args>(args)...);
-    }
-    template <typename T, typename... Args>
-    explicit arena(const in_place_type_t<T>, Args&&... args)
-        : arena(in_place_index<index_of<T, Ts...>>, std::forward<Args>(args)...)
-    {
-    }
-    arena()  // Type #0 must be default-constructible, otherwise this type is not default-constructible either.
-        : arena(in_place_index<0>)
-    {
-    }
-
-    /// Constructs a new value in the arena. The old value will be destroyed beforehand by calling destroy().
-    /// If the constructor throws, the arena will be left valueless.
-    template <std::size_t N, typename T = nth_type<N, Ts...>, typename... Args>
-    auto& construct(Args&&... args)
-    {
-        destroy();                                                    // First, make it valueless...
-        auto* const p = new (m_data) T(std::forward<Args>(args)...);  // If constructor throws, stay valueless.
-        m_index       = N;                                            // If constructor succeeds, become non-valueless.
-        m_dtor        = [](void* const data) { reinterpret_cast<T*>(data)->~T(); };
-        return *p;
-    }
-    template <typename T, typename... Args>
-    auto& construct(Args&&... args)
-    {
-        return construct<index_of<T, Ts...>>(std::forward<Args>(args)...);
-    }
-
-    /// Destroys the value and makes this arena valueless. Does nothing if the arena is already valueless.
-    /// If all Ts are trivially destructible, there is no need to invoke this function upon destruction of the arena.
-    void destroy() noexcept
-    {
-        if (!is_valueless())
-        {
-            m_index = variant_npos;
-            assert(m_dtor != nullptr);
-            m_dtor(m_data);
-            m_dtor = nullptr;
-        }
-        assert(is_valueless());
-    }
-
-    /// Returns variant_npos if valueless.
-    CETL_NODISCARD std::size_t index() const noexcept
-    {
-        return m_index;
-    }
-
-    CETL_NODISCARD bool is_valueless() const noexcept
-    {
-        return m_index == variant_npos;
-    }
-
-    // clang-format off
-    template <typename T>       T&  as()      &  { return *reinterpret_cast<T*>(m_data); }
-    template <typename T> const T&  as() const&  { return *reinterpret_cast<const T*>(m_data); }
-    template <typename T>       T&& as()      && { return std::move(*reinterpret_cast<T*>(m_data)); }
-    template <typename T> const T&& as() const&& { return std::move(*reinterpret_cast<const T*>(m_data)); }
-
-    template <std::size_t N>       auto&  as()      &  { return as<nth_type<N, Ts...>>(); }
-    template <std::size_t N> const auto&  as() const&  { return as<nth_type<N, Ts...>>(); }
-    template <std::size_t N>       auto&& as()      && { return as<nth_type<N, Ts...>>(); }
-    template <std::size_t N> const auto&& as() const&& { return as<nth_type<N, Ts...>>(); }
-    // clang-format on
-
-private:
-    alignas(std::max({alignof(Ts)...})) unsigned char m_data[std::max({sizeof(Ts)...})];
-    // We could have used visitation instead but the closure approach does not require runtime search.
-    void        (*m_dtor)(void*);
-    std::size_t m_index;
-};
-static_assert(std::is_trivially_copyable<arena<int, double>>::value, "");
-static_assert(std::is_trivially_copy_constructible<arena<int, double>>::value, "");
-static_assert(std::is_trivially_move_constructible<arena<int, double>>::value, "");
-static_assert(std::is_trivially_copy_assignable<arena<int, double>>::value, "");
-static_assert(std::is_trivially_move_assignable<arena<int, double>>::value, "");
-static_assert(std::is_trivially_destructible<arena<int, double>>::value, "");
-
 /// DESTRUCTION POLICY
 template <typename Seq, bool = Seq::trivially_destructible>
 struct base_destruction;
 /// Trivially destructible case.
 template <typename... Ts>
-struct base_destruction<types<Ts...>, true>
+struct base_destruction<types<Ts...>, true>  // NOLINT(*-pro-type-member-init)
 {
-    arena<Ts...> m_arena;
+    /// If the constructor throws, the instance will be left valueless.
+    template <std::size_t N, typename T = nth_type<N, Ts...>, typename... Args>
+    auto& construct(Args&&... args)
+    {
+        destroy();
+        auto* const p = new (m_data) T(std::forward<Args>(args)...);  // If constructor throws, stay valueless.
+        m_index       = N;                                            // If constructor succeeds, become non-valueless.
+        return *p;
+    }
+    void destroy()
+    {
+        m_index = variant_npos;  // The destructor is trivial.
+    }
+    alignas(std::max({alignof(Ts)...})) unsigned char m_data[std::max({sizeof(Ts)...})];
+    std::size_t m_index = variant_npos;
 };
 /// Non-trivially destructible case.
 template <typename... Ts>
-struct base_destruction<types<Ts...>, false>
+struct base_destruction<types<Ts...>, false>  // NOLINT(*-pro-type-member-init)
 {
     base_destruction()                                   = default;
     base_destruction(const base_destruction&)            = default;
@@ -199,9 +121,29 @@ struct base_destruction<types<Ts...>, false>
     base_destruction& operator=(base_destruction&&)      = default;
     ~base_destruction() noexcept
     {
-        m_arena.destroy();
+        m_dtor(m_data);
     }
-    arena<Ts...> m_arena;
+    /// If the constructor throws, the instance will be left valueless.
+    template <std::size_t N, typename T = nth_type<N, Ts...>, typename... Args>
+    auto& construct(Args&&... args)
+    {
+        destroy();
+        auto* const p = new (m_data) T(std::forward<Args>(args)...);  // If constructor throws, stay valueless.
+        m_index       = N;                                            // If constructor succeeds, become non-valueless.
+        m_dtor        = [](void* const data) { reinterpret_cast<T*>(data)->~T(); };
+        return *p;
+    }
+    void destroy()
+    {
+        m_dtor(m_data);
+        m_dtor  = [](void*) {};
+        m_index = variant_npos;
+    }
+    alignas(std::max({alignof(Ts)...})) unsigned char m_data[std::max({sizeof(Ts)...})];
+    std::size_t m_index = variant_npos;
+
+private:
+    void (*m_dtor)(void*) = [](void*) {};
 };
 
 /// COPY CONSTRUCTION POLICY
@@ -211,18 +153,77 @@ struct base_copy_construction;
 template <typename... Ts>
 struct base_copy_construction<types<Ts...>, true> : base_destruction<types<Ts...>>
 {
-    // Copies constructed by merely copying the bytes of the source.
+    using base = base_destruction<types<Ts...>>;
+    using base::construct;
 };
 /// Non-trivially copy constructible case.
 template <typename... Ts>
 struct base_copy_construction<types<Ts...>, false> : base_destruction<types<Ts...>>
 {
-    base_copy_construction()                                         = default;
-    base_copy_construction(const base_copy_construction&)            = default;
+    using base               = base_destruction<types<Ts...>>;
+    base_copy_construction() = default;
+    base_copy_construction(const base_copy_construction& other)
+        : m_copy_ctor(other.m_copy_ctor)
+    {
+        m_copy_ctor(this->m_data, other.m_data);  // May throw. No-op if the other is valueless.
+        this->m_index = other.m_index;
+    }
     base_copy_construction(base_copy_construction&&)                 = default;
     base_copy_construction& operator=(const base_copy_construction&) = default;
     base_copy_construction& operator=(base_copy_construction&&)      = default;
     ~base_copy_construction() noexcept                               = default;
+    /// If the constructor throws, the instance will be left valueless.
+    template <std::size_t N, typename T = nth_type<N, Ts...>, typename... Args>
+    auto& construct(Args&&... args)
+    {
+        m_copy_ctor = [](void*, const void*) {};  // Ground in case we become valueless.
+        auto& res   = base::template construct<N>(std::forward<Args>(args)...);
+        m_copy_ctor = [](void* const dst, const void* const src) { new (dst) T(*reinterpret_cast<const T*>(src)); };
+        return res;
+    }
+
+private:
+    void (*m_copy_ctor)(void*, const void*) = [](void*, const void*) {};
+};
+
+/// MOVE CONSTRUCTION POLICY
+template <typename Seq, bool = Seq::trivially_move_constructible>
+struct base_move_construction;
+/// Trivially move constructible case.
+template <typename... Ts>
+struct base_move_construction<types<Ts...>, true> : base_copy_construction<types<Ts...>>
+{
+    using base = base_copy_construction<types<Ts...>>;
+    using base::construct;
+};
+/// Non-trivially move constructible case.
+template <typename... Ts>
+struct base_move_construction<types<Ts...>, false> : base_copy_construction<types<Ts...>>
+{
+    using base                                            = base_copy_construction<types<Ts...>>;
+    base_move_construction()                              = default;
+    base_move_construction(const base_move_construction&) = default;
+    base_move_construction(base_move_construction&& other) noexcept(types<Ts...>::nothrow_move_constructible)
+        : m_move_ctor(other.m_move_ctor)
+    {
+        m_move_ctor(this->m_data, other.m_data);  // May throw. No-op if the other is valueless.
+        this->m_index = other.m_index;
+    }
+    base_move_construction& operator=(const base_move_construction&) = default;
+    base_move_construction& operator=(base_move_construction&&)      = default;
+    ~base_move_construction() noexcept                               = default;
+    /// If the constructor throws, the instance will be left valueless.
+    template <std::size_t N, typename T = nth_type<N, Ts...>, typename... Args>
+    auto& construct(Args&&... args)
+    {
+        m_move_ctor = [](void*, void*) {};  // Ground in case we become valueless.
+        auto& res   = base::template construct<N>(std::forward<Args>(args)...);
+        m_move_ctor = [](void* const dst, void* const src) { new (dst) T(std::move(*reinterpret_cast<T*>(src))); };
+        return res;
+    }
+
+private:
+    void (*m_move_ctor)(void*, void*) = [](void*, void*) {};
 };
 
 }  // namespace var
