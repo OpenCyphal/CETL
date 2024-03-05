e43b3faf9352c911d24e751bc64f81c74ca6c75c
Pavel Kirienko <pavel.kirienko@gmail.com>
2024-03-05 14:54:19 +0200

    Adjust the docs

--- include/cetl/type_traits_ext.hpp
+++ include/cetl/type_traits_ext.hpp
@@ -1,5 +1,6 @@
 /// @file
-/// Type traits extensions not found in C++ standards.
+/// Type traits extensions not found in C++ standards. This is mostly needed for internal use in the library,
+/// but can also be useful for users.
 /// @copyright
 /// Copyright (C) OpenCyphal Development Team  <opencyphal.org>
 /// Copyright Amazon.com Inc. or its affiliates.
@@ -38,23 +39,32 @@ struct find_impl<Predicate, Ix, Head, Tail...> : find_impl<Predicate, Ix + 1, Ta
 };
 }  // namespace detail
 
-/// Index of the first type T in the typelist for which Predicate<T>::value is true.
+/// The \c value member contains the index of the first type T in the typelist for which Predicate<T>::value is true.
 template <template <typename...> class Predicate, typename... Ts>
 struct find : std::integral_constant<std::size_t, detail::find_impl<Predicate, 0, Ts...>::first_match_index>
 {};
+/// Alias for \c find.
 template <template <typename...> class Predicate, typename... Ts>
 static constexpr std::size_t find_v = find<Predicate, Ts...>::value;
 
-/// Number of types in the typelist for which Predicate<T>::value is true.
+/// The \c value member contains the number of types in the typelist for which Predicate<T>::value is true.
 template <template <typename...> class Predicate, typename... Ts>
 struct count : std::integral_constant<std::size_t, detail::find_impl<Predicate, 0, Ts...>::match_count>
 {};
+/// Alias for \c count.
 template <template <typename...> class Predicate, typename... Ts>
 static constexpr std::size_t count_v = count<Predicate, Ts...>::value;
 
 // --------------------------------------------------------------------------------------------
 
 /// Partially apply a template by holding the Left type arguments fixed.
+/// The resulting template is accessible via the member template alias named \c type.
+/// For correct usage don't forget to prefix the \c type member with \c template; e.g.:
+/// @code
+/// template <typename T, typename... Ts>
+/// constexpr std::size_t first_index_of_v =
+///     type_traits_ext::find_v<type_traits_ext::partial<std::is_same, T>::template type, Ts...>;
+/// @endcode
 /// Does not work with non-type template parameters.
 template <template <typename...> class F, typename... Left>
 struct partial
