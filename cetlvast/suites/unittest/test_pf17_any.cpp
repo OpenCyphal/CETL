2a18dab6892a46434266d4cd12b7a6666406aad1
Sergei Shirokov <sergei@Sergeis-Mac-mini.local>
2024-03-18 08:26:45 +0200

    Implemented some `any` constructors (#1, 4) and `emplace`, `~any`, `reset` `has_value` methods.
    Also first revision of standalone `any_cast` functions (without yet RTTI).

--- cetlvast/suites/unittest/test_pf17_any.cpp
+++ cetlvast/suites/unittest/test_pf17_any.cpp
@@ -13,34 +13,110 @@
 namespace
 {
 
-/// ------------------------------------------------------------------------------------------------
+using cetl::pf17::any;
+using cetl::pf17::any_cast;
+using cetl::pf17::bad_any_cast;
+
+/// TESTS -----------------------------------------------------------------------------------------------------------
 
 TEST(test_bad_any_cast, ctor)
 {
     // Test the default constructor.
-    cetl::pf17::bad_any_cast test_exception1;
+    bad_any_cast test_exception1;
 
     // Test the copy constructor.
-    cetl::pf17::bad_any_cast test_exception2{test_exception1};
+    bad_any_cast test_exception2{test_exception1};
 
     // Test the move constructor.
-    cetl::pf17::bad_any_cast test_exception3{std::move(test_exception2)};
+    bad_any_cast test_exception3{std::move(test_exception2)};
     EXPECT_STREQ("bad_any_cast", test_exception3.what());
 }
 
 TEST(test_bad_any_cast, assignment)
 {
     // Test the copy assignment operator.
-    cetl::pf17::bad_any_cast test_exception1;
-    cetl::pf17::bad_any_cast test_exception2;
+    bad_any_cast test_exception1;
+    bad_any_cast test_exception2;
     test_exception2 = test_exception1;
 
     // Test the move assignment operator.
-    cetl::pf17::bad_any_cast test_exception3;
+    bad_any_cast test_exception3;
     test_exception3 = std::move(test_exception2);
     EXPECT_STREQ("bad_any_cast", test_exception3.what());
 }
 
 /// ------------------------------------------------------------------------------------------------
+
+TEST(test_any, cppref_example)
+{
+    using uut = any<sizeof(int)>;
+
+    uut  a{1};
+    auto ptr = any_cast<int>(&a);
+    EXPECT_TRUE(ptr);
+    EXPECT_EQ(1, *ptr);
+
+    // TODO: Add more from the example when corresponding api will be available.
+    // https://en.cppreference.com/w/cpp/utility/any
+}
+
+TEST(test_any, ctor_1_default)
+{
+    EXPECT_FALSE((any<0>{}.has_value()));
+    EXPECT_FALSE((any<0, false>{}.has_value()));
+    EXPECT_FALSE((any<0, false, true>{}.has_value()));
+    EXPECT_FALSE((any<0, true, false>{}.has_value()));
+
+    EXPECT_FALSE((any<1>{}.has_value()));
+    EXPECT_FALSE((any<1, false>{}.has_value()));
+    EXPECT_FALSE((any<1, false, true>{}.has_value()));
+    EXPECT_FALSE((any<1, true, false>{}.has_value()));
+
+    EXPECT_FALSE((any<13>{}.has_value()));
+    EXPECT_FALSE((any<13, false>{}.has_value()));
+    EXPECT_FALSE((any<13, false, true>{}.has_value()));
+    EXPECT_FALSE((any<13, true, false>{}.has_value()));
+}
+
+TEST(test_any, any_cast_4_const_ptr)
+{
+    using uut = const any<sizeof(int)>;
+
+    uut a{147};
+
+    auto intPtr = any_cast<int>(&a);
+    static_assert(std::is_same_v<const int*, decltype(intPtr)>);
+
+    EXPECT_TRUE(intPtr);
+    EXPECT_EQ(147, *intPtr);
+
+    EXPECT_FALSE((any_cast<char, uut>(nullptr)));
+}
+
+TEST(test_any, any_cast_5_non_const_ptr)
+{
+    using uut = any<sizeof(char)>;
+
+    uut a{'Y'};
+
+    auto charPtr = any_cast<char>(&a);
+    static_assert(std::is_same_v<char*, decltype(charPtr)>);
+    EXPECT_TRUE(charPtr);
+    EXPECT_EQ('Y', *charPtr);
+
+    EXPECT_FALSE((any_cast<char, uut>(nullptr)));
+}
+
+TEST(test_any, function_value)
+{
+    // TODO: Try put/get function.
+    GTEST_SKIP() << "Implement me!";
+}
+
+TEST(test_any, lambda_value)
+{
+    // TODO: Try put/get lambda.
+    GTEST_SKIP() << "Implement me!";
+}
 
 }  // namespace
--- include/cetl/pf17/any.hpp
+++ include/cetl/pf17/any.hpp
@@ -10,6 +10,8 @@
 
 #include <cetl/pf17/attribute.hpp>
 
+#include <algorithm>
+#include <type_traits>
 #include <typeinfo>   // We need this for `std::bad_cast`.
 #include <exception>  // We need this even if exceptions are disabled for `std::terminate`.
 
@@ -17,6 +19,9 @@ namespace cetl  // NOLINT(*-concat-nested-namespaces) TODO: Fix this when C++17.
 {
 namespace pf17
 {
+
+// Forward declarations
+template <std::size_t Footprint, bool Copyable, bool Movable>
 class any;
 
 #if defined(__cpp_exceptions) || defined(CETL_DOXYGEN)
@@ -40,6 +45,206 @@ public:
 };
 
 #endif  // defined(__cpp_exceptions) || defined(CETL_DOXYGEN)
+
+namespace
+{
+
+// Move policy.
+template <std::size_t Footprint, bool Copyable, bool Movable>
+struct base_move;
+
+// Copy policy.
+template <std::size_t Footprint, bool Copyable>
+struct base_copy;
+
+template <std::size_t Footprint, std::size_t Align = sizeof(std::max_align_t)>
+struct base_storage
+{
+    // We need to align the buffer to the given value (maximum alignment by default).
+    // Also, we need to ensure that the buffer is at least 1 byte long.
+    alignas(Align) char buffer_[std::max(Footprint, 1UL)];
+};
+
+// Movable case.
+template <std::size_t Footprint, bool Copyable>
+struct base_move<Footprint, Copyable, true> : base_copy<Footprint, Copyable>
+{
+public:
+    constexpr base_move()                      = default;
+    base_move(base_move&&) noexcept            = default;
+    base_move& operator=(base_move&&) noexcept = default;
+};
+
+// Non-movable case.
+template <std::size_t Footprint, bool Copyable>
+struct base_move<Footprint, Copyable, false> : base_copy<Footprint, Copyable>
+{
+public:
+    constexpr base_move()                      = default;
+    base_move(base_move&&) noexcept            = delete;
+    base_move& operator=(base_move&&) noexcept = delete;
+};
+
+// Copyable case.
+template <std::size_t Footprint>
+struct base_copy<Footprint, true> : base_storage<Footprint>
+{
+public:
+    constexpr base_copy()                  = default;
+    base_copy(const base_copy&)            = default;
+    base_copy& operator=(const base_copy&) = default;
+};
+
+// Non-copyable case.
+template <std::size_t Footprint>
+struct base_copy<Footprint, false> : base_storage<Footprint>
+{
+public:
+    constexpr base_copy()                  = default;
+    base_copy(const base_copy&)            = delete;
+    base_copy& operator=(const base_copy&) = delete;
+};
+
+enum class action
+{
+    Get,
+    Destroy
+};
+
+}  // namespace
+
+template <std::size_t Footprint, bool Copyable = true, bool Movable = Copyable>
+class any : private base_move<Footprint, Copyable, Movable>
+{
+public:
+    constexpr any() noexcept = default;
+
+    template <typename ValueType, typename Tp = std::decay_t<ValueType>>
+    any(ValueType&& value)
+    {
+        soo_handler<Tp>::create(*this, std::forward<ValueType>(value));
+    }
+
+    ~any()
+    {
+        reset();
+    }
+
+    template <typename ValueType, typename... Args, typename Tp = std::decay_t<ValueType>>
+    Tp& emplace(Args&&... args)
+    {
+        reset();
+        return soo_handler<Tp>::create(*this, std::forward<Args>(args)...);
+    }
+
+    void reset() noexcept
+    {
+        handle(action::Destroy);
+    }
+
+    CETL_NODISCARD bool has_value() const noexcept
+    {
+        return handler_ != nullptr;
+    }
+
+private:
+    // Type-erased handler.
+    using any_handler = void* (*) (action, const any* /*self*/);
+
+    // Small Object Optimization (SOO) handler.
+    template <typename Tp>
+    struct soo_handler
+    {
+        static_assert(sizeof(Tp) <= Footprint, "Enlarge the footprint");
+
+        static void* handle(action action, const any* self)
+        {
+            switch (action)
+            {
+            case action::Get:
+                return get(const_cast<any&>(*self));
+            case action::Destroy:
+                destroy(const_cast<any&>(*self));
+                return nullptr;
+            }
+        }
+
+        template <typename... Args>
+        static Tp& create(any& dest, Args&&... args)
+        {
+            Tp* ret = static_cast<Tp*>(static_cast<void*>(&dest.buffer_));
+            new (ret) Tp(std::forward<Args>(args)...);
+            dest.handler_ = handle;
+            return *ret;
+        }
+
+    private:
+        static void* get(any& self)
+        {
+            // TODO: Add RTTI check here.
+            return static_cast<void*>(&self.buffer_);
+        }
+
+        static void destroy(any& self)
+        {
+            Tp* ptr = static_cast<Tp*>(static_cast<void*>(&self.buffer_));
+            ptr->~Tp();
+            self.handler_ = nullptr;
+        }
+
+    };  // struct soo_handler
+
+    template <typename ValueType, typename Any>
+    friend std::add_pointer_t<ValueType> any_cast(Any* operand) noexcept;
+
+    void* handle(action action) const
+    {
+        return handler_ ? handler_(action, this) : nullptr;
+    }
+
+    any_handler handler_ = nullptr;
+
+};  // class any
+
+/// \brief Performs type-safe access to the `const` contained object.
+///
+/// \tparam ValueType Type of the requested value; may not be a reference.
+/// \tparam Any Type of the `any` object.
+/// \param operand Target constant any object.
+/// \return If operand is not a null pointer,
+///     and the typeid of the requested `ValueType` matches that of the contents of operand,
+///     a pointer to the value contained by operand, otherwise a null pointer.
+///
+template <typename ValueType, typename Any>
+std::add_pointer_t<std::add_const_t<ValueType>> any_cast(const Any* operand) noexcept
+{
+    return any_cast<ValueType>(const_cast<Any*>(operand));
+}
+
+/// \brief Performs type-safe access to the contained object.
+///
+/// \tparam ValueType Type of the requested value; may not be a reference.
+/// \tparam Any Type of the `any` object.
+/// \param operand Target `any` object.
+/// \return If operand is not a null pointer,
+///     and the typeid of the requested `ValueType` matches that of the contents of operand,
+///     a pointer to the value contained by operand, otherwise a null pointer.
+///
+template <typename ValueType, typename Any>
+std::add_pointer_t<ValueType> any_cast(Any* operand) noexcept
+{
+    static_assert(!std::is_reference<ValueType>::value, "`ValueType` may not be a reference.");
+
+    if (!operand)
+    {
+        return nullptr;
+    }
+
+    void* ptr = operand->handle(action::Get);
+
+    using ReturnType = std::add_pointer_t<ValueType>;
+    return static_cast<ReturnType>(ptr);
+}
 
 }  // namespace pf17
 }  // namespace cetl
